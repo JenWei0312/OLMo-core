77fbd35dad866e2ba8e0037c985f0268f64b0b03
Jen Wei <jenwei0312@gmail.com>
2026-05-02 18:41:26 -0600

    uncomment enable_plan_caching as input

--- src/olmo_core/distributed/checkpoint/__init__.py
+++ src/olmo_core/distributed/checkpoint/__init__.py
@@ -78,7 +78,7 @@ def save_state_dict(
     thread_count: Optional[int] = None,
     process_count: Optional[int] = None,
     throttle_uploads: bool = False,
-    #enable_plan_caching: bool = False,
+    enable_plan_caching: bool = False,
     _skip_prepare: bool = False,
 ):
     """
