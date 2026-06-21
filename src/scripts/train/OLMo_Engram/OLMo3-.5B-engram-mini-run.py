7ce98b7e6709ec13633fdeac79c53022114230d5
Jen Wei <jenwei0312@gmail.com>
2026-06-21 12:13:35 -0600

    debugging run on base model with 2xA100

--- src/scripts/train/OLMo_Engram/OLMo3-.5B-engram-mini-run.py
+++ src/scripts/train/OLMo_Engram/OLMo3-.5B-engram-mini-run.py
@@ -91,9 +91,9 @@ BASE_MODEL = True
 
 if TRAIN_FOR_DEBUG:
     # Force tight telemetry logging to inspect every single step
-    WARMUP_STEPS = 10 # 10 or 20 depending on  integration or debugging run
-    METRICS_INTERVAL = 5 # 5 or 10, not necessarily every step, depending on integration or debugging run
-    MAX_DURATION = Duration.steps(20) # 20 or 200, depending on integration or debugging run
+    WARMUP_STEPS = 20 # 10 or 20 depending on  integration or debugging run
+    METRICS_INTERVAL = 10 # 5 or 10, not necessarily every step, depending on integration or debugging run
+    MAX_DURATION = Duration.steps(200) # 20 or 200, depending on integration or debugging run
     EVAL_INTERVAL =  100
     EVAL_ON_FINISH =  True
 else:
@@ -110,7 +110,7 @@ else:
 # ==========================================
 SEQUENCE_LENGTH = 2048
 GLOBAL_BATCH_SIZE = 32 * SEQUENCE_LENGTH  # Token-constant batch size
-RANK_MICROBATCH_SIZE = 4 * SEQUENCE_LENGTH  # Sequence size per card
+RANK_MICROBATCH_SIZE = 16 * SEQUENCE_LENGTH  # Sequence size per card
 
 LR = 3e-4
 WEIGHT_DECAY = 0.1
