6dfff662962032e761394888724787d7ff7a3748
Jen Wei <jenwei0312@gmail.com>
2026-09-14 12:26:32 -0600

    try dion3 with constant scheduler

--- src/scripts/train/Optim/OLMo3-.5B-optim-mini-run.py
+++ src/scripts/train/Optim/OLMo3-.5B-optim-mini-run.py
@@ -89,6 +89,7 @@ from olmo_core.optim import (
     Dion3Config,
     CustomEngramDionConfig,  # < -- Custom engram_dion, clean public import!
     CosWithWarmup,
+    ConstantScheduler,
 )
 from olmo_core.train import Duration, TrainerConfig
 from olmo_core.eval import Evaluator # <-- Add Evaluator here
@@ -391,7 +392,8 @@ def build_train_module_config(
         float8_config=Float8Config(enabled=False),
         z_loss_multiplier=1e-5,
         max_grad_norm=1.0,
-        scheduler=CosWithWarmup(warmup_steps=profile.warmup_steps),
+        #scheduler=CosWithWarmup(warmup_steps=profile.warmup_steps),
+        scheduler = ConstantScheduler(),
     )
 
 # ==========================================
@@ -521,7 +523,7 @@ if __name__ == "__main__":
     # ==========================================
     RUN_TYPE       = "integration"         # "integration" (20 steps) | "debug" (200 steps) | "production" (5B tokens)
     MODEL_TYPE     = "dense_base"   # "dense_base" | "engram_attn" | "engram_gdn"
-    OPTIMIZER_TYPE = "dion"         # "adamw" | "muon" | "dion"| "dion3"
+    OPTIMIZER_TYPE = "dion3"         # "adamw" | "muon" | "dion"| "dion3"
 
     # 1. Resolve duration profile
     profile = build_train_profile(RUN_TYPE)
